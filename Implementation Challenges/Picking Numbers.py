f22657e160835da0069dee7e23d7bea0031bfe0b
Harshil <darjiharshil2994@gmail.com>
2017-12-23 19:34:48 +0530

    Dec 23 17 07:34

--- Implementation Challenges/Picking Numbers.py
+++ Implementation Challenges/Picking Numbers.py
@@ -2,24 +2,18 @@
 # https://www.hackerrank.com/challenges/picking-numbers/problem
 
 n = int(input().strip())
-arr = [int(i) for i in input().split()][:n]
+array = [int(i) for i in input().split()][:n]
 
-counts = {}
-sortedCounts = []
+counts = []
+for i in set(sorted(array)):
+    counts.append([i, array.count(i)])
+
 answer = 0
-
-for num in arr:
-    counts[num] = counts.get(num, 0) + 1
- 
-for k,v in sorted(counts.items()):
-    sortedCounts.append([k, v])
-
-length = len(sortedCounts)
-for i in range(length):
-    if i < length - 1:
-        if sortedCounts[i + 1][0] - sortedCounts[i][0] < 2:
-            totalCount = sortedCounts[i][1] + sortedCounts[i + 1][1]
-            answer = totalCount if totalCount > answer else answer
-    answer = sortedCounts[i][1] if sortedCounts[i][1] > answer else answer
- 
+for i in range(len(counts)):
+    if i < len(counts) - 1:
+        if counts[i+1][0] - counts[i][0] <= 1:
+            total = counts[i][1] + counts[i+1][1]
+            answer = total if total > answer else answer
+    answer = counts[i][1] if counts[i][1] > answer else answer
 print(answer)
+        
